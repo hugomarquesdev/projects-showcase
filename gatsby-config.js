5eb5c7577b02a86aa94aba1f25a1e1efe02c46ef
Hugo Marques <62065869+hugomarquesdev@users.noreply.github.com>
2025-04-22 18:15:57 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,14 +54,6 @@ module.exports = {
         queries: myCustomQueries,
       },
     },
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "",
-        includeInDevelopment: false,
-        defaultDataLayer: { platform: "gatsby" },
-      },
-    },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     // `gatsby-transformer-json`,
@@ -88,35 +80,6 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-htaccess`,
     `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-facebook-pixel`,
-      options: {
-        pixelId: "",
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "",
-      },
-    },
     `gatsby-plugin-remove-serviceworker`,
     {
       resolve: `gatsby-plugin-react-i18next`,
